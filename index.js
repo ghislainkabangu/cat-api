307d23cd3ec90dada1c87b5845bc9d4e56f9b136
ghislain kabangu <kabangughislain77@gmail.com>
2024-03-27 21:06:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let imageCat = document.getElementById("image");
+  let id = document.querySelector(".id");
+  let cat = fetch(
+    "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1"
+  )
+    .then((resultat) => resultat.json())
+    .then((data) => {
+      imageCat.src = data[0].url;
+      console.log(data);
+      id.innerHTML = "Identifiant : " + data[0].id;
+    });
+});
