fb268844e7a7265aab9457bcf0d0b9a5f665748a
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-03-28 13:43:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /** @format */
 
 let button = document.getElementById("button");
+let blocImage = document.getElementById("blocImage");
 button.addEventListener("click", () => {
   let imageCat = document.getElementById("image");
   let id = document.querySelector(".id");
+
+  blocImage.classList.add("my-img-container");
   let cat = fetch(
     "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1"
   )
@@ -11,5 +14,6 @@ button.addEventListener("click", () => {
     .then((data) => {
       imageCat.src = data[0].url;
       id.innerHTML = "Identifiant : " + data[0].id;
+     
     });
 });
